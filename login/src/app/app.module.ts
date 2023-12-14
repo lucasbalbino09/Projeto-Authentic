2810d2f31daf14582b4afc2b6b75c2ce5e1bb305
lucas.balbino <balbino2409@gmail.com>
2023-12-14 15:53:29 -0300

    subindo continuação do projeto

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SignComponent } from './core/components/pages/sign/sign.component';
-
-
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignComponent,
-    
+    AppComponent   
   ],
   imports: [
     BrowserModule,
