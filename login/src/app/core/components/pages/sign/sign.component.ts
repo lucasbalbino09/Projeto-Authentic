8976dcdd80e5ae3440f94b6cfefcd28398b9302e
lucas.balbino <balbino2409@gmail.com>
2023-12-14 15:07:09 -0300

    subindo projeto

---
+++ login/src/app/core/components/pages/sign/sign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html',
+  styleUrls: ['./sign.component.less']
+})
+export class SignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
